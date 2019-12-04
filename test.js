29b9ec7bc941c8dc7048445eb69011c478ce7d4f
E24046894 <wu.yu.chen@outlook.com>
2019-12-04 10:30:17 +0800

    page

---
+++ test.js
@@ -0,0 +1,14 @@
+
+$("#to_two").click(function () {
+    $("#pg_one").css("opacity", 0);
+    $("#pg_two").css("opacity", 1);
+    $("#pg_three").css("opacity", 0);
+});
+
+$("#to_three").click(function () {
+    $("#pg_one").css("opacity", 0);
+    $("#pg_two").css("opacity", 0);
+    $("#pg_three").css("opacity", 1);
+});
+
+
