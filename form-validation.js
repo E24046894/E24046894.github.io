2f1c9f73a1e8971040a45d4e85cd90e5ebd899dc
E24046894 <wu.yu.chen@outlook.com>
2019-12-18 08:39:33 +0800

    final

--- form-validation.js
+++ form-validation.js
@@ -2,17 +2,21 @@
 (function () {
   'use strict'
 
-  window.addEventListener('load', function () {
+  window.addEventListener('load', function ()
+  {
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.getElementsByClassName('needs-validation')
 
     // Loop over them and prevent submission
-    Array.prototype.filter.call(forms, function (form) {
-      form.addEventListener('submit', function (event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
+    Array.prototype.filter.call(forms, function (form)
+    {
+        form.addEventListener('submit', function (event)
+        {
+            if (form.checkValidity() === false)
+            {
+              event.preventDefault()
+              event.stopPropagation()
+            }
         form.classList.add('was-validated')
       }, false)
     })
--- test.js
+++ test.js
@@ -1,14 +1,34 @@
 
-$("#to_two").click(function () {
-    $("#pg_one").css("opacity", 0);
-    $("#pg_two").css("opacity", 1);
+$(document).ready(function () {
+    $("#pg_one").css("opacity", 1);
+    $("#pg_two").css("opacity", 0);
     $("#pg_three").css("opacity", 0);
 });
 
-$("#to_three").click(function () {
+$("#to_two").click(function () {
     $("#pg_one").css("opacity", 0);
+    $("#pg_two").css("opacity", 1);
+    
+    $("#pg_one").offset({ top: 5000 });
+    $("#pg_two").offset({ top: 0 });
+
+});
+
+$("#to_three").click(function (e) {
     $("#pg_two").css("opacity", 0);
     $("#pg_three").css("opacity", 1);
+    e.preventDefault();
+
+    $("#pg_two").offset({ top: 5000 });
+    $("#pg_three").offset({ top: 0 });
 });
 
 
+$("#complete").click(function () {
+    $("#pg_one").css("opacity", 1);
+    $("#pg_two").css("opacity", 0);
+    $("#pg_three").css("opacity", 0);
+
+    $("#pg_three").offset({ top: 5000 });
+    $("#pg_one").offset({ top: 0 });
+});
